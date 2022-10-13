87878824b45855cfca74a8d75b4818de2cf3da8f
Lis of sky <lisovskiy_90@inbox.ru>
2022-10-13 22:16:21 +0300

    g

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-let finger = document.querySelector('.vibr');
-console.log("finger", finger)
+let btn = document.querySelector('.vibra');
+
+function vibrate (e) {
+    e.preventDefault();
+    navigator.vibrate([300, 100, 300, 100]);
+}
+
+btn.addEventListener("touch", (e) => {
+    e.preventDefault();
+    vibrate()
+}
+
+)
 
 function vibrFinger(ms) {
-    navigator.vibrate(ms)
+    window.navigator.vibrate(ms)
 }
